84f6d127074fb03a7f717855ae6f5e4e1a8e5a3e
--global <--global>
2022-07-25 20:59:57 +0530

    changes

--- Frontend/src/components/HelloWorld.vue
+++ Frontend/src/components/HelloWorld.vue
@@ -34,7 +34,16 @@
 </table>
 </template>
 <script>
+import Vue from 'vue'
 import axios from 'axios'
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 export default{
 data() {
   return {
