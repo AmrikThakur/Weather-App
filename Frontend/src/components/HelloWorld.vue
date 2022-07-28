0367faacd75892e8642d7045779a527f9f275a9e
Amrik Singh <amrikthakur1996@gmail.com>
2022-07-28 12:34:29 +0530

    previous code

--- Frontend/src/components/HelloWorld.vue
+++ Frontend/src/components/HelloWorld.vue
@@ -34,15 +34,15 @@
 </table>
 </template>
 <script>
-import Vue from 'vue'
+// import Vue from 'vue'
 import axios from 'axios'
 
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+// Vue.use(BootstrapVue)
+// Vue.use(IconsPlugin)
 
 export default{
 data() {
