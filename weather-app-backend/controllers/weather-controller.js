a47980ee7cbfc785bd65e98a549478975d7d7cab
Amrik Singh <amrikthakur1996@gmail.com>
2022-08-03 11:32:56 +0530

    some changes in backend stuff

--- weather-app-backend/controllers/weather-controller.js
+++ weather-app-backend/controllers/weather-controller.js
@@ -1,35 +1,37 @@
 const asyncMiddleware = require('../middleware/async')
-const axios =require('axios')
-const { json } = require('express')
+const axios = require('axios')
+
+// API for Get Current Weather
 exports.currentWeather = asyncMiddleware(async (req, res) => {
 
-let city_id=req.body.city_id
-if(!city_id)return res.status(400).send({message:"City id is required"})
+    let city_id = req.body.city_id
+    if (!city_id) return res.status(400).send({ message: "City id is required" })
 
- let weatherData=await axios.get(`http://api.openweathermap.org/data/2.5/weather?id=${city_id}&appid=538882fc8387290c6cee83f313a6acf5`)
+    let weatherData = await axios.get(`http://api.openweathermap.org/data/2.5/weather?id=${city_id}&appid=538882fc8387290c6cee83f313a6acf5`)
 
-  const current_weather = weatherData.data;
+    const current_weather = weatherData.data;
 
-  return res.status(200).send({
-         message: "Weather Fetched Successfully",
-         values:current_weather
-     })
+    return res.status(200).send({
+        message: "Weather Fetched Successfully",
+        values: current_weather
+    })
 })
 
 
+// API for Get ForcastWeather
 
 exports.forCastWeather = asyncMiddleware(async (req, res) => {
 
-    let city_id=req.body.city_id
-    if(!city_id)return res.status(400).send({message:"City id is required"})
-    
- let forecastData = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?id=${city_id}&appid=538882fc8387290c6cee83f313a6acf5`);
+    let city_id = req.body.city_id
+    if (!city_id) return res.status(400).send({ message: "City id is required" })
+
+    let forecastData = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?id=${city_id}&appid=538882fc8387290c6cee83f313a6acf5`);
     const data = forecastData.data;
-    
-      return res.status(200).send({
-             message: "ForCast Weather Fetched Successfully",
-             values:data
-         })
+
+    return res.status(200).send({
+        message: "ForCast Weather Fetched Successfully",
+        values: data
+    })
 })
 
 
--- weather-app-backend/test/weather-controller.test.js
+++ weather-app-backend/test/weather-controller.test.js
@@ -11,13 +11,33 @@ test('check currentWeather api include city Id', async () => {
     expect(city_id).not.toBe('')
 });
 
-
-test('check forCastWeather api giving success response', async () => {
-    const { city_id } = req.body;
-    expect(city_id).not.toBe('')
-})
-
-test('check currentWeather api include city Id', async () => {
+test('check currentWeather api give success status', async () => {
     await currentWeather(req, res, next);
     expect(res.status).toBeCalledWith(200);
 });
+
+test('check forCastWeather api give success status', async () => {
+    await forCastWeather(req, res, next);
+    expect(res.status).toBeCalledWith(200);
+});
+
+test('check forCastWeather api include city Id', async () => {
+    const { city_id } = req.body;
+    expect(city_id).not.toBe('')
+})    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
